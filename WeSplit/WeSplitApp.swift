33b4851f7821072df7d962caf82a8dcd2d9d8900
Geoff <geoffreylewis78@gmail.com>
2024-11-06 18:12:22 +0000

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Geoff Lewis on 06/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
